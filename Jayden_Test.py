897e671dff9cd2d1accc9b1c1dc927f9cc25f377
Jayden Sorensen <jayden.sorensen@outlook.com>
2022-10-08 15:29:02 -0600

    Add a loop to drive pin high and low

--- Jayden_Test.py
+++ Jayden_Test.py
@@ -1,9 +1,13 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
-GPIO.setwarnings(False) 
+GPIO.setwarnings(True)
  
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(40, GPIO.OUT)
 
 while (True):
     GPIO.output(40, True) 
+    sleep(2)
+    GPIO.output(40, False)
+    sleep(2)

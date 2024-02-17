f99faa0e1ec448c60f4c7790dda2fd73e67d1451
Jesse Gerbrandt <jgerbrandt@live.com>
2024-02-17 16:37:34 -0700

    Fixing sidebar, adding background, subscribing sliders to db

--- RocketControlUnitGUI/src/routes/+page.svelte
+++ RocketControlUnitGUI/src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-	import { getModalStore, initializeStores, SlideToggle } from '@skeletonlabs/skeleton';
+	import { getModalStore, SlideToggle } from '@skeletonlabs/skeleton';
 	import type { ModalSettings } from '@skeletonlabs/skeleton';
 	import { currentState } from '../store';
-    import PocketBase from 'pocketbase';
 	import { onMount } from 'svelte';
     import { writable } from 'svelte/store';
     import type { RecordModel } from 'pocketbase';
-	// import { write } from 'fs';
+	import back from '$lib/assets/test.svg';
+	import PocketBase from 'pocketbase';
 
 	const modalStore = getModalStore();
+	
+	const PB = new PocketBase("http://127.0.0.1:8090");
 
 	let nextStatePending: string = '';
 	function confirmStateChange(state: string): void {
@@ -55,15 +57,22 @@
 	// Create a writable store to hold the PadBoxStatus data
 	export const padBoxStatus = writable<RecordModel | null>(null);
 
-	let PB = new PocketBase("http://127.0.0.1:8090");
+	let ac1_open: boolean = false;
+	let ac2_open: boolean = false;
+	let pbv1_open: boolean = false;
+	let pbv2_open: boolean = false;
+	let pbv3_open: boolean = false;
 
-	let ac1_checked: boolean = false;
-	let ac2_checked: boolean = false;
-	let pbv1_checked: boolean = false;
-	let pbv2_checked: boolean = false;
 	let power_enable_checked: boolean = false;
-	let sol1_checked: boolean = false;
-	let sol2_checked: boolean = false;
+	let sol1_open: boolean = false;
+	let sol2_open: boolean = false;
+	let sol3_open: boolean = false;
+	let sol4_open: boolean = false;
+	let sol5_open: boolean = false;
+	let sol6_open: boolean = false;
+	let sol7_open: boolean = false;
+	let sol8a_open: boolean = false;
+	let sol8b_open: boolean = false;
 	let padbox_cont1_checked: boolean = false;
 	let padbox_cont2_checked: boolean = false;
 	let padbox_box1_checked: boolean = false;
@@ -97,8 +106,8 @@
 		const resultList = await PB.collection('RelayStatus').getList(1, 1, {
 			sort: '-created',
 		});
-
-		ac1_open = resultList.items[0].ac1_open
+		
+    	ac1_open = resultList.items[0].ac1_open;
 		ac2_open = resultList.items[0].ac2_open
 		pbv1_open = resultList.items[0].pbv1_open
 		pbv2_open = resultList.items[0].pbv2_open
@@ -112,32 +121,22 @@
 		sol7_open = resultList.items[0].sol7_open
 		sol8a_open = resultList.items[0].sol8a_open
 		sol8b_open = resultList.items[0].sol8b_open
-
-
-				
-		// PB.collection('RelayStatus').subscribe('ac1_open', function (e) {
-		// 	// Update the PadBoxStatus data store whenever a change is detected
-		// 	ac1_checked=e.record.ac1_open;
-		// 	// relayStatus.ac1_open = e.record.ac1_open;
-		// });
 	})
 
-	$: ac1_open = $relayStatus?.ac1_open || false;
-	$: ac2_open = $relayStatus?.ac2_open || false;
-	$: pbv1_open = $relayStatus?.pbv1_open || false;
-	$: pbv2_open = $relayStatus?.pbv2_open || false;
-	$: pbv3_open = $relayStatus?.pbv3_open || false;
-	$: sol1_open = $relayStatus?.sol1_open || false;
-	$: sol2_open = $relayStatus?.sol2_open || false;
-	$: sol3_open = $relayStatus?.sol3_open || false;
-	$: sol4_open = $relayStatus?.sol4_open || false;
-	$: sol5_open = $relayStatus?.sol5_open || false;
-	$: sol6_open = $relayStatus?.sol6_open || false;
-	$: sol7_open = $relayStatus?.sol7_open || false;
-	$: sol8a_open = $relayStatus?.sol8a_open || false;
-	$: sol8b_open = $relayStatus?.sol8b_open || false;
-
-
+	// $: ac1_open = $relayStatus?.ac1_open || false;	
+	// $: ac2_open = $relayStatus?.ac2_open || false;
+	// $: pbv1_open = $relayStatus?.pbv1_open || false;
+	// $: pbv2_open = $relayStatus?.pbv2_open || false;
+	// $: pbv3_open = $relayStatus?.pbv3_open || false;
+	// $: sol1_open = $relayStatus?.sol1_open || false;
+	// $: sol2_open = $relayStatus?.sol2_open || false;
+	// $: sol3_open = $relayStatus?.sol3_open || false;
+	// $: sol4_open = $relayStatus?.sol4_open || false;
+	// $: sol5_open = $relayStatus?.sol5_open || false;
+	// $: sol6_open = $relayStatus?.sol6_open || false;
+	// $: sol7_open = $relayStatus?.sol7_open || false;
+	// $: sol8a_open = $relayStatus?.sol8a_open || false;
+	// $: sol8b_open = $relayStatus?.sol8b_open || false;
 
 	function writePadBoxChange() {
 		// Create a change on the 'PadBoxStatus' collection
@@ -154,12 +153,12 @@
 		// Create a change on the 'RelayStatus' collection
 		PB.collection('RelayStatus').create ({
 			// Write a new record with all current values
-			'ac1_open': ac1_checked,
-			'ac2_open': ac2_checked,
-			'pbv1_open': pbv1_checked,
-			'pbv2_open': pbv2_checked,
-			'sol1_open': sol1_checked,
-			'sol2_open': sol2_checked
+			'ac1_open': ac1_open,
+			'ac2_open': ac2_open,
+			'pbv1_open': pbv1_open,
+			'pbv2_open': pbv2_open,
+			'sol1_open': sol1_open,
+			'sol2_open': sol2_open
 		});
 	}
 
@@ -173,15 +172,13 @@
 
 	function handleAC1Change(e: any) {
 		const target = e.target;
-    	ac1_checked = target.checked;
-    	console.log(ac1_checked);
-
 		writeRelayStatusChange();
+    	ac1_open = target.checked;
 	}
 
 	function handleAC2Change(e: any) {
 		const target = e.target;
-    	ac2_checked = target.checked;
+    	ac2_open = target.checked;
     	console.log(target.checked);
 
 		writeRelayStatusChange();
@@ -189,7 +186,7 @@
 
 	function handlePBV1Change(e: any) {
 		const target = e.target;
-    	pbv1_checked = target.checked;
+    	pbv1_open = target.checked;
     	console.log(target.checked);
 
 		writeRelayStatusChange();
@@ -197,7 +194,7 @@
 
 	function handlePBV2Change(e: any) {
 		const target = e.target;
-    	pbv2_checked = target.checked;
+    	pbv2_open = target.checked;
     	console.log(target.checked);
 
 		writeRelayStatusChange();
@@ -213,7 +210,7 @@
 
 	function handleSOL1Change(e: any) {
 		const target = e.target;
-    	sol1_checked = target.checked;
+    	sol1_open = target.checked;
     	console.log(target.checked);
 
 		writeRelayStatusChange();
@@ -221,7 +218,7 @@
 
 	function handleSOL2Change(e: any) {
 		const target = e.target;
-    	sol2_checked = target.checked;
+    	sol2_open = target.checked;
     	console.log(target.checked);
 
 		writeRelayStatusChange();
@@ -259,17 +256,11 @@
 		writePadBoxChange();
 	}
 
-
-
 </script>
 
-
-
-<svelte:head></svelte:head>
-
-
 <main> 
-	<h1>HELLO</h1>
+	<div id="background" style="background-image: url({back}); background-repeat: no-repeat; background-size: 100%; background-position: center top; position:fixed; top: 0; left: 0; right:0; bottom:0; width: 100%; height: 100%;"></div>
+
 	<SlideToggle name="ac1_slider" bind:checked={ac1_open} on:change={handleAC1Change}> AC1 {$relayStatus && 'ac1_open' in $relayStatus ? $relayStatus.ac1_open : 'N/A'}</SlideToggle>
 	<SlideToggle name="ac2_slider" bind:checked={ac2_open} on:change={handleAC2Change}> AC2 {$relayStatus && 'ac2_open' in $relayStatus ? $relayStatus.ac2_open : 'N/A'}</SlideToggle>
 
@@ -279,18 +270,15 @@
 	<SlideToggle name="power_enable_slider" on:change={handlePowerEnableChange}> Power Enable {$combustionControlStatus && 'mev_power_enable' in $combustionControlStatus ? $combustionControlStatus.mev_power_enable : 'N/A'}</SlideToggle>
 
 	<SlideToggle name="sol1_slider" bind:checked={sol1_open} on:change={handleSOL1Change}> SOL1 {$relayStatus && 'sol1_open' in $relayStatus ? $relayStatus.sol1_open : 'N/A'}</SlideToggle>
-	<SlideToggle name="sol2_slider" bind:checked={sol2_open} on:change={handleSOL2Change}> SOL2 {$relayStatus && 'sol2_open' in $relayStatus ? $relayStatus.sol2_open : 'N/A'}</SlideToggle>
+	<SlideToggle name="sol2_slider" bind:checked={sol2_open} on:change={handleSOL2Change}> SOL2 {$relayStatus && 'sol2_open' in $relayStatus ? $relayStatus.sol2_open : 'N/A'}</SlideToggle> -->
 
 	<SlideToggle name="padbox_cont1_slider" on:change={handleCont1Change}> Cont1 {$padBoxStatus && 'cont1' in $padBoxStatus ? $padBoxStatus.cont1 : 'N/A'}</SlideToggle>
 	<SlideToggle name="padbox_cont2_slider" on:change={handleCont2Change}> Cont2 {$padBoxStatus && 'cont2' in $padBoxStatus ? $padBoxStatus.cont2 : 'N/A'}</SlideToggle>
 	<SlideToggle name="padbox_box1_slider" on:change={handleBox1Change}> Box1 {$padBoxStatus && 'box1_on' in $padBoxStatus ? $padBoxStatus.box1_on : 'N/A'}</SlideToggle>
 	<SlideToggle name="padbox_box2_slider" on:change={handleBox2Change}> Box2 {$padBoxStatus && 'box2_on' in $padBoxStatus ? $padBoxStatus.box2_on : 'N/A'}</SlideToggle>
 
-
 	<h1>TC1 {$rcuTemp && 'tc1_temp' in $rcuTemp ? $rcuTemp.tc1_temp: 'N/A'}</h1>
 	<h1>TC2 {$rcuTemp && 'tc2_temp ' in $rcuTemp ? $rcuTemp.tc2_temp : 'N/A'}</h1>
-
-
 	
 	<!-- Render different buttons based on the current state -->
 	{#if $currentState === states.RS_PRELAUNCH}
@@ -353,28 +341,13 @@
 	{/if}
 </main>
 
-
 <style>
+	#background {
+		z-index: -1;
+	}
 	.next-state-btn {
 		position: fixed;
 		left: 100px;
 		width: 200px;
-	}
-
-	@keyframes glow {
-		0% {
-			box-shadow:
-				0 0 5px #00ff00,
-				0 0 10px #00ff00,
-				0 0 15px #00ff00,
-				0 0 20px #00ff00;
-		}
-		100% {
-			box-shadow:
-				0 0 10px #00ff00,
-				0 0 20px #00ff00,
-				0 0 30px #00ff00,
-				0 0 40px #00ff00;
-		}
 	}
 </style>

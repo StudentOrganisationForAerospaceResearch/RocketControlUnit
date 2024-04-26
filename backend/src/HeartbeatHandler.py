e711d76c96d8520576cc018d63d6c2035e1dd99b
Jesse Gerbrandt <jgerbrandt@live.com>
2024-04-25 22:00:24 -0600

    reworking dmb heartbeat send to restart properly after hb loss

--- backend/src/HeartbeatHandler.py
+++ backend/src/HeartbeatHandler.py
@@ -16,7 +16,8 @@ class HeartbeatHandler:
         HeartbeatHandler.thread_name = thread_name
         self.timeout = timeout
         self.timer = None
-        self.running = True
+        self.running = False
+        self.dmb_thread = None
 
     def reset_timer(self):
         if self.timer is not None:
@@ -39,11 +40,37 @@ class HeartbeatHandler:
 
     def stop(self):
         self.running = False
+        if self.dmb_thread is not None:
+            self.dmb_thread.join()
+            self.dmb_thread = None
         if self.timer is not None:
             self.timer.cancel()
 
     def start(self):
+        self.running = True
+        self.dmb_thread = threading.Thread(target=self.dmb_heartbeat_send)
+        self.dmb_thread.start()
         self.reset_timer()
+
+    def dmb_heartbeat_send(self):
+        while self.running:
+            msg = ControlMessage()
+            msg.source = ProtoCore.NODE_RCU
+            msg.target = ProtoCore.NODE_DMB
+            hbMsg = Heartbeat()
+            hbMsg.hb_response_sequence_num = 1234
+            msg.hb.CopyFrom(hbMsg)
+
+            self.send_message_workq.put(
+                WorkQ_Message(
+                    self.thread_name,
+                    'radio', 
+                    THREAD_MESSAGE_HEARTBEAT_SERIAL,
+                    (msg.SerializeToString(),)
+                )
+            )
+            time.sleep(5)
+
 
 def heartbeat_thread(thread_name: str, heartbeat_workq: mp.Queue, message_handler_workq: mp.Queue):
     """
@@ -55,31 +82,10 @@ def heartbeat_thread(thread_name: str, heartbeat_workq: mp.Queue, message_handle
     heartbeat_handler = HeartbeatHandler(thread_name, heartbeat_workq, message_handler_workq, timeout_seconds)
     heartbeat_handler.start()
 
-    dmb_heartbeat_send_thread = threading.Thread(target=dmb_heartbeat_send, args=(heartbeat_handler,))   
-    dmb_heartbeat_send_thread.start()
-
     while (1):
         if not process_workq_message(heartbeat_workq.get(block=True), heartbeat_handler):
             return
 
-def dmb_heartbeat_send(heartbeat_handler: HeartbeatHandler):
-    while heartbeat_handler.running:
-        msg = ControlMessage()
-        msg.source = ProtoCore.NODE_RCU
-        msg.target = ProtoCore.NODE_DMB
-        hbMsg = Heartbeat()
-        hbMsg.hb_response_sequence_num = 1234
-        msg.hb.CopyFrom(hbMsg)
-
-        heartbeat_handler.send_message_workq.put(
-            WorkQ_Message(
-                heartbeat_handler.thread_name,
-                'radio', 
-                THREAD_MESSAGE_HEARTBEAT_SERIAL,
-                (msg.SerializeToString(),)
-            )
-        )
-        time.sleep(5)
 
 def process_workq_message(message: WorkQ_Message, heartbeat_handler: HeartbeatHandler) -> bool:
     """
@@ -94,9 +100,10 @@ def process_workq_message(message: WorkQ_Message, heartbeat_handler: HeartbeatHa
 
     if messageID == THREAD_MESSAGE_HEARTBEAT:
         logger.debug(f"Resetting Heartbeat Timer")
-        heartbeat_handler.reset_timer()
         if heartbeat_handler.running == False:
-            heartbeat_handler.running = True
+            heartbeat_handler.start()
+        else:
+            heartbeat_handler.reset_timer()
         return True
     else:
         logger.error(f"Unknown message type: {messageID}")

bf866fbacd1469a893224643b9dc357effbf2823
connellr023 <connell.reffo@ucalgary.ca>
2024-10-26 11:53:03 -0600

    Pause functionality is now working!

--- RocketControlUnitGUI/src/lib/components/PausablePromptModal.svelte
+++ RocketControlUnitGUI/src/lib/components/PausablePromptModal.svelte
@@ -1,27 +1,39 @@
+<script lang="ts" context="module">
+    export type PausablePromptResponse = ["submit" | "cancel" | "pause" | undefined, string];
+</script>
+
 <script lang="ts">
 	import { getModalStore } from '@skeletonlabs/skeleton';
 
     const modalStore = getModalStore();
 
     export let heading: string = "";
-    export let inputMethod: "text" | "number" = "text";
+    let inputValue: string = "";
 
-    const finish = (value: "submit" | "cancel" | "pause") => {
-        $modalStore[0].response!(value);
+    const finish = (value: PausablePromptResponse) => {
+        $modalStore[0]?.response!(value);
+        modalStore.close();
+    }
+
+    const submit = () => {
+        finish(["submit", inputValue]);
+    }
+
+    const pause = () => {
+        finish(["pause", inputValue]);
     }
 
     const cancel = () => {
-        modalStore.close();
-        finish("cancel");
+        finish(["cancel", ""]);
     }
 </script>
 
 <div class="">
     <header>{heading}</header>
-    <input type={inputMethod} />
+    <input bind:value={inputValue} type="text" />
     <div class="modal-actions">
-        <button on:click={() => finish("submit")}>Submit</button>
-        <button on:click={() => finish("pause")}>Pause</button>
+        <button on:click={submit}>Submit</button>
+        <button on:click={pause}>Pause</button>
         <button on:click={cancel}>Cancel</button>
     </div>
 </div>
